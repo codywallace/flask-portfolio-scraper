3fba79e8489e1746a0f4d218928195d327fdc59d
codywallace <cwall.0059@gmail.com>
2025-02-18 14:16:18 -0500

    url encoding for un website

--- job_scraper/config.py
+++ job_scraper/config.py
@@ -1,6 +1,12 @@
+import json
+import urllib.parse
+
+data = {"jle":["100"],"ds":[]}
+encoded_data = urllib.parse.quote(json.dumps(data))
+
 UNDP_URL = "https://jobs.undp.org/cj_view_jobs.cfm"
 UNICEF_URL = "https://jobs.unicef.org/en-us/filter/?search-keyword=&pay-scale=p-3"
-UN_Secretariat_URL = "https://careers.un.org/jobopening?language=en&data=%257B%2522aoe%2522:%255B%255D,%2522aoi%2522:%255B%255D,%2522el%2522:%255B%255D,%2522ct%2522:%255B%255D,%2522ds%2522:%255B%2522NEWYORK%2522%255D,%2522jn%2522:%255B%255D,%2522jf%2522:%255B%255D,%2522jc%2522:%255B%2522PD%2522%255D,%2522jle%2522:%255B%2522100%2522%255D,%2522dept%2522:%255B%255D,%2522span%2522:%255B%255D%257D"
+UN_Secretariat_URL = "https://careers.un.org/jobopening?language=en&data={encoded_data}"
 
 HEADERS_LIST = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
                 "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
